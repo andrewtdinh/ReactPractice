718daf5a176715b8a1c30f1b3b27ff385f3599ef
Andrew Dinh <andrewdinh74@yahoo.com>
2015-07-26 20:56:24 -0700

    Initial

---
+++ 001/script.jsx
@@ -0,0 +1,20 @@
+var HelloWorld = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
+
+  increment: function() {
+    this.setState({ counter: this.state.counter+1 });
+  },
+
+  render: function() {
+    return <div>
+      <div>{this.state.counter}</div>
+      <button onClick={this.increment}>Increment!</button>
+    </div>;
+  }
+});
+
+React.render(new HelloWorld(), document.body);
